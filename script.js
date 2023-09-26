e2d96fbea5b8be384883a30b9f97d00d2d1cd600
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-26 17:42:17 -0400

    Began work on clear and one buttons

---
+++ script.js
@@ -0,0 +1,11 @@
+const display = document.querySelector(".display");
+const content = document.querySelector(".content");
+const buttons = document.querySelectorAll(".buttons");
+const one = document.querySelector(".one");
+const clear = document.querySelector(".clear");
+one.addEventListener("click", () => {
+    
+});
+clear.addEventListener("click", () => {
+    content.textContent = 0;
+})
