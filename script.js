b1cfc0a156cfe1b65eee6173c4d74dc39b1cae68
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-27 13:48:26 -0400

    Created add, substract, multiply, divide functions

--- script.js
+++ script.js
@@ -3,9 +3,30 @@ const content = document.querySelector(".content");
 const buttons = document.querySelectorAll(".buttons");
 const one = document.querySelector(".one");
 const clear = document.querySelector(".clear");
+content.textContent = 0;
 one.addEventListener("click", () => {
     
 });
 clear.addEventListener("click", () => {
     content.textContent = 0;
-})
+});
+
+const add = function add(num1,num2) {
+    return result = num1+num2;
+    console.log(result);
+};
+
+const substract = function substract(num1,num2) {
+    return result = num1-num2;
+    console.log(result);
+};
+
+const multiply = function multiply(num1,num2) {
+    return result = num1*num2;
+    console.log(result);
+};
+
+const divide = function divide(num1,num2) {
+    return result = num1/num2;
+    console.log(result);
+};
